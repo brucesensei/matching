81cfa570d46d1998f77b6ecf71c930280b4201f5
nolan bruce <nolan_76@msn.com>
2023-03-19 21:40:10 +0900

    Added minimal data for category list and images. implemented category choice route and image choice route. Image data displays ranomdized in the game route with blocks to cover the images.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+//-----------------------REQUIRE PACKAGES-----------------------
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const compression = require("compression");
+const fs = require("fs");
+//-----------------------USE PACKAGES----------------------------
 
 const app = express();
 app.use(helmet());
@@ -10,15 +14,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//---------------------CATEGORY ARRAY---------------------------
+
+const choices = ["animals", "nature", "months"];
+
+//------------------VARIABLES TO PASS WITH EJS------------------
+
+let category = "";
+let imageArray = [];
+let players = "";
+let userChoices = [];
+
+//--------------------APP ROUTES--------------------------------
+
 app.get("/", function(req,res) {
-  console.log("implements index get route")
-  res.render("pages/index");
+  res.render("pages/index", { choices: choices });
 })
 
 app.post("/", function(req, res) {
-  console.log("implements the index post route");
-  console.log("hello variables.");
+  category = req.body.categorylist;
+  imageArray = fs.readdirSync(`public/images/${category}/`);
+  res.redirect("/select");
 })
+
+app.get("/select", function(req, res) {
+  res.render("pages/select", { imageArray: imageArray, category: category });
+})
+
+app.post("/select", function(req, res) {
+  let userPics = req.body;
+  let arrayKey = Object.keys(userPics)[1]
+  players = req.body.players;
+  const firstArray = userPics[arrayKey];
+  const secondArray = [...firstArray];
+  const joinedArrays = [...firstArray, ...secondArray];
+  userChoices = joinedArrays.sort((a, b) => 0.5 - Math.random());
+  res.redirect("/game");
+})
+
+app.get("/game", function(req, res) {
+  res.render("pages/game", { players: players, userChoices: userChoices, category: category });
+})
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("server running on port 3000");
